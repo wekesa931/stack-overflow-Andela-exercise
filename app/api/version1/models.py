3683be3c7a0b012495ff13bd83ce0780d92dd033
Wekesa Bill <wekesabill@gmail.com>
2018-12-20 08:02:07 -0500

    [starts #162784857] adds creates the correct folder structure]

---
+++ app/api/version1/models.py
@@ -0,0 +1,16 @@
+#Miscellaneous operating system interfaces
+#Allows us to interact with the underlying operating system in different ways e.g updating a file or changing directory
+#import os
+
+users = {
+    1:{
+        "name":"Bill",
+        "email":"wekesa@gmail.com",
+        "age":70
+    },
+    2:{
+        "name":"John",
+        "email":"john@gmail.com",
+        "age":90
+    }
+}

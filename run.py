ab739eb9ff4490b61a33ee0f7a460d2d9b555763
Wekesa Bill <wekesabill@gmail.com>
2018-12-20 07:08:14 -0500

    debug mode activated

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ config_name = os.getenv("APP_SETTINGS")
 app = create_app(config_name)
 """defining the configuration to be used"""
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
